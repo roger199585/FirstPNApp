51bbdcecdf2500fbc7f1780d43a40068d2f07e4c
Corn <roger199585@yahoo.com.tw>
2016-08-19 14:20:44 +0800

    add a inputtext and show it on the screen

--- index.android.js
+++ index.android.js
@@ -6,17 +6,54 @@
 
 import React, { Component } from 'react';
 import {
+/*
+  你所需要用到的標籤要先在這裡定義
+*/
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  TextInput
 } from 'react-native';
 
 class FirstPNApp extends Component {
+  //constructor(props) {
+  //  super(props)
+  //}
+  // 系統預設圍上方那樣  若要自給複寫須記得加入super(props)
+  
+  
+  constructor(props) {
+    super(props)
+    this.state = {
+      text: 'Corn',
+      text2: 'haha'
+      //若此處將text指定為固定值  則畫面上只能限是此質無法進行改變
+      //因此如果你要讓你的textinput可以更改上面的職的話須加入onchangetext    
+      
+    }
+  }
+  onPress = () => {
+    console.log('Hello Cor123n~')
+  }
+  // onPress can't work
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
+        <Text>
+          Hello I'm Corn
+        </Text>
+        
+        <TextInput 
+          style={{backgroundColor: 'black', color: 'white', textAlign: 'center', width: 120, height:40}}
+          onChangeText = {(text) => this.setState({text})} value={this.state.text}></TextInput>
+        <Text> Hello! {this.state.text} {this.state.text2} ~ </Text>
+        <TextInput 
+          style={{backgroundColor: 'gray', color: 'white', textAlign: 'center', width: 120, height:40}}
+          onChangeText = {(text2) => this.setState({text2})} value={this.state.text2}></TextInput>
+        <Text> Hello! {this.state.text2} ~</Text>
+        
+        <Text style={styles.welcome} onPress={this.onPress} >
           Welcome to React Native!
         </Text>
         <Text style={styles.instructions}>
